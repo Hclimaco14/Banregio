a049f746aa0f86565a5a0382f36d8ee0ef8564b4
Hector Climaco <hector.gpe.climaco@gmail.com>
2022-09-08 12:27:48 -0500

    Initial Commit

---
+++ DemoBanRegio/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoBanRegio
+//
+//  Created by Hector Climaco on 08/09/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
